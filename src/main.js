62caab902b1377ba71fbe87ddfa01f3c769aced9
Roberto Peixinho <rhpeixinho@gmail.com>
2022-10-21 14:23:33 -0300

    feature: adicionados eventos

--- src/main.js
+++ src/main.js
@@ -1,40 +1,27 @@
 import "./css/index.css"
 import IMask from "imask";
 
-const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path');
-const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path');
-const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img');
-
-function setCardType(type) {
-    const colors = {
-        "visa": ["#436D99", "#2D57F2"],
-        "mastercard": ["#DF6F29", "#C69347"],
-        "default": ["black", "gray"]
-    }
-    
-    ccBgColor01.setAttribute('fill', colors[type][0]);
-    ccBgColor02.setAttribute('fill', colors[type][1]);
-    ccLogo.setAttribute('src', `cc-${type}.svg`);
-}
-
 
 const securityCode = document.querySelector('#security-code');
 const securityCodePattern = {
     mask: "000",
-    lazy: false
 };
 const securityCodeMasked = IMask(securityCode, securityCodePattern);
 
-securityCode.addEventListener('keyup', (e) => {
-    const securityCodeValue = document.querySelector('.cc-security .value');
-    securityCodeValue.innerHTML = securityCodeMasked.value;
+securityCodeMasked.on('accept', () => {
+    updateSecurityCode(securityCodeMasked.value)
 })
+
+function updateSecurityCode (code) {
+    const securityCodeValue = document.querySelector('.cc-security .value');
+
+    securityCodeValue.innerHTML = code ? code : "123";
+}
 
 
 const expirationDate = document.querySelector('#expiration-date');
 const expirationDatePattern = {
     mask: "MM{/}YY",
-    lazy: false,
     blocks: {
         MM : {
             mask: IMask.MaskedRange,
@@ -50,10 +37,15 @@ const expirationDatePattern = {
 };
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
 
-expirationDate.addEventListener('keyup', (e) => {
-    const expirationDateValue = document.querySelector('.cc-expiration .value');
-    expirationDateValue.innerHTML = expirationDateMasked.value;
+expirationDateMasked.on('accept', () => {
+    updateExpirationDate(expirationDateMasked.value);
 })
+
+function updateExpirationDate (date) {
+    const securityCodeValue = document.querySelector('.cc-expiration .value');
+
+    securityCodeValue.innerHTML = date ? date : "02/32";
+}
 
 
 const cardNumber = document.querySelector('#card-number');
@@ -61,19 +53,16 @@ const cardNumberPattern = {
     mask: [
         {
             mask: "0000 0000 0000 0000",
-            lazy: false,
             regex: /^4\d{0,15}/,
             cardtype: "visa"
         },
         {
             mask: "0000 0000 0000 0000",
-            lazy: false,
             regex:/^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2}\d{0,12}/,
             cardtype: "mastercard"
         },
         {
             mask: "0000 0000 0000 0000",
-            lazy: false,
             cardtype: "default"
         }
     ],
@@ -84,13 +73,42 @@ const cardNumberPattern = {
         return foundMask;
     }
 }
-
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
 
-cardNumber.addEventListener('keyup', (e) => {
-    const cardNumberValue = document.querySelector('.cc-number');
-    cardNumberValue.innerHTML = cardNumberMasked.value;
+cardNumberMasked.on('accept', () => {
+    updateCardNumber(cardNumberMasked.value);
+    setCardType(cardNumberMasked.masked.currentMask.cardtype);
 
+})
+
+function setCardType(type) {
+    const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path');
+    const ccBgColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path');
+    const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img');
+    const colors = {
+        "visa": ["#436D99", "#2D57F2"],
+        "mastercard": ["#DF6F29", "#C69347"],
+        "default": ["black", "gray"]
+    }
+    
+    ccBgColor01.setAttribute('fill', colors[type][0]);
+    ccBgColor02.setAttribute('fill', colors[type][1]);
+    ccLogo.setAttribute('src', `cc-${type}.svg`);
+}
+
+function updateCardNumber (number) {
+    const cardNumberValue = document.querySelector('.cc-number');
+
+    cardNumberValue.innerHTML = number ? number : "1234 5678 9012 3456";
+}
+
+
+const cardHolder = document.querySelector('#card-holder');
+
+cardHolder.addEventListener('keyup', () => {
+    const cardHolderValue = document.querySelector('.cc-holder .value');
+
+    cardHolderValue.innerHTML = cardHolder.value ? cardHolder.value : "FULANO DA SILVA";
 })
 
 
